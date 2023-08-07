2832eb3ed3133f7d5e95029ddfef8e332dcad440
Sravan Kumar <75154116+Etikala7607@users.noreply.github.com>
2023-08-06 21:22:22 -0500

    Add files via upload

---
+++ workshop/aks.tf
@@ -0,0 +1,47 @@
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "my_resource"
+    storage_account_name = "newworkshop66"
+    container_name       = "workshop"
+    key                  = "terraform.tfstate"
+  }
+}
+
+
+resource "azurerm_resource_group" "default" {
+  name     = "my-resource-group"
+  location = "East US"  # Replace with your desired Azure region
+}
+
+resource "azurerm_kubernetes_cluster" "default" {
+  name                = "my-aks-cluster"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+  dns_prefix          = "my-aks-cluster"
+
+  kubernetes_version  = "1.26.3"  # Replace with your desired Kubernetes version
+
+  default_node_pool {
+    name            = "default"
+    node_count      = 2
+    vm_size         = "Standard_D2_v2"
+    os_disk_size_gb = 30
+  }
+
+  service_principal {
+    client_id     = var.appId  # Replace with your service principal client ID
+    client_secret = var.password  # Replace with your service principal client secret
+  }
+
+  role_based_access_control_enabled = true
+
+  tags = {
+    environment = "Demo"
+  }
+}
+
+
---
+++ workshop/terraform.tfvars
@@ -0,0 +1,3 @@
+
+appId    = "11824412-7f92-41eb-a7b1-2fc0bfe62b59"
+password = "uy18Q~EIC-gDUgyVtVJpEQ1VCMHHBMKhp.-xccYq"
